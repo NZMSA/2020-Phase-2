afbccd0b126e1f5b3fbc3f5835f5ecfd2858420d
Neeraj <neerajghodla111@gmail.com>
2020-08-04 19:56:08 +1200

    set up the basic database structure

---
+++ phase-2-back-end/phase-2-back-end/Database/ApplicationDatabase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace phase_2_back_end.Database
+{
+    public class ApplicationDatabase : DbContext
+    {
+        public ApplicationDatabase(DbContextOptions<ApplicationDatabase> options) : base(options)
+        {
+
+        }
+    }
+}
