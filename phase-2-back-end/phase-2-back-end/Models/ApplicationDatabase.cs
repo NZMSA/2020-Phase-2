7125aa5e3134d121f3ff01b9a39f8dbb8a1bf710
Nguyen Nguyen <nguyennguyen6395@gmail.com>
2020-08-19 17:51:03 +1200

    Update db.cs context with onconfiguring method

--- phase-2-back-end/phase-2-back-end/Models/ApplicationDatabase.cs
+++ phase-2-back-end/phase-2-back-end/Models/ApplicationDatabase.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,10 +11,17 @@ namespace phase_2_back_end.Database
     {
         public ApplicationDatabase(DbContextOptions<ApplicationDatabase> options) : base(options)
         {
-
         }
 
         public DbSet<Canvas> Canvas { get; set; }
         public DbSet<ColorData> ColorData { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+           .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+           .AddJsonFile("appsettings.json")
+           .Build();
+        }
     }
 }
