75c1a720d90c731b18a4941a717e58b28f67ca43
Jayson <jayson.tai@hotmail.com>
2020-08-27 08:27:46 +1200

    Header added to root

--- phase-2-front-end/src/App.tsx
+++ phase-2-front-end/src/App.tsx
@@ -3,16 +3,19 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import HistoricalGrid from "./components/HistoricalGrid/HistoricalGrid";
 import LatestGrid from "./components/LatestGrid/LatestGrid";
+import Header from './components/Header/Header';
 
 import "./App.css";
 
 const App = () => {
-  return <BrowserRouter>
+  return <div>
+    <Header/><BrowserRouter>
   <Switch>
     <Route path='/history' component={HistoricalGrid} />
     <Route path='/' component={LatestGrid} />
   </Switch>
   </BrowserRouter>
+    </div>
 };
 
 export default App;
